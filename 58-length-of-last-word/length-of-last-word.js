60239f1e868e2fdda8ae6cbd656884cbfadd976b
Cyber-Knight <56642237+Cyber-Knightt@users.noreply.github.com>
2025-08-06 20:08:58 +0530

    Time: 1 ms (11.54%) | Memory: 53.7 MB (48.62%) - LeetSync

---
+++ 58-length-of-last-word/length-of-last-word.js
@@ -0,0 +1,16 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var lengthOfLastWord = function (s) {
+    let length = 0;
+    let trims = s.trim()
+    for (var i = trims.length - 1; i >= 0; i--) {
+        if (trims[i] == " ") {
+            console.log('length-----', s, trims[i])
+            break;
+        }
+        length++;
+    }
+    return length;
+};
