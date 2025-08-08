5a16fefc2b95ea3df763037c856eb41cc5ea7dea
Cyber-Knight <56642237+Cyber-Knightt@users.noreply.github.com>
2025-08-08 14:12:35 +0530

    Time: 1 ms (72.61%) | Memory: 55.6 MB (71.95%) - LeetSync

--- 69-sqrtx/sqrtx.js
+++ 69-sqrtx/sqrtx.js
@@ -2,22 +2,21 @@
  * @param {number} x
  * @return {number}
  */
-var mySqrt = function(x) {
-    let low  = 1;
+var mySqrt = function (x) {
+    if (x == 0) return 0;
+    let low = 1;
     let high = x;
     let result = 1;
-    if(x==0) return 0;
-    while(low<=high){
-      let mid = Math.floor((high+low)/2)      
-      if (mid*mid == x) {
-        return mid;
-      }
-      if(mid*mid < x) {
-        low = mid+1;
-        result = mid;
-      }else {
-        high = mid-1;
-      }
+    while (low <= high) {
+        let mid = Math.floor((high + low) / 2)
+        if (mid * mid == x) {
+            return mid;
+        } else if (mid * mid < x) {
+            low = mid + 1;
+            result = mid;
+        } else {
+            high = mid - 1;
+        }
     }
     return result;
 };
