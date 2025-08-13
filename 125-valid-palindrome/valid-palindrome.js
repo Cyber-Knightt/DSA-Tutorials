ca67b689bfb85b9abf0cac3d168174a395229a94
Cyber-Knight <56642237+Cyber-Knightt@users.noreply.github.com>
2025-08-13 17:22:51 +0530

    Time: 5 ms (64.26%) | Memory: 56.7 MB (79.93%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.js
+++ 125-valid-palindrome/valid-palindrome.js
@@ -4,7 +4,7 @@
  */
 var isPalindrome = function(s) {
     let str = s.trim().toLowerCase().replace(/[^a-zA-Z0-9]/g, '');
-    if(str.lemgth == 0) return true;
+    if(str.length == 0) return true;
     let left=0;
     let right = str.length-1;
     
