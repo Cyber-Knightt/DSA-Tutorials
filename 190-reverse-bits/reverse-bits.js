dbe6d9465f0b76bcae3ebca4f289ecd7929ffc15
Cyber-Knight <56642237+Cyber-Knightt@users.noreply.github.com>
2025-08-20 11:36:05 +0530

    Time: 41 ms (82.24%) | Memory: 54.6 MB (50.16%) - LeetSync

--- 190-reverse-bits/reverse-bits.js
+++ 190-reverse-bits/reverse-bits.js
@@ -5,7 +5,6 @@
 var reverseBits = function (n) {
     let binaryNum = n.toString(2).padStart(32, '0');
     let reverseBinary = binaryNum.split('').reverse();
-    let num = reverseBinary.join('');
-    let parseNum = parseInt(num, 2) >>> 0;
-    return parseNum;
+    let num = parseInt(reverseBinary.join(''), 2) >>> 0;
+    return num;
 };
