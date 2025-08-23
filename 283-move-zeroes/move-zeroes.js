4b54b4a9db6379729696cf426f6d5864c20fc4ad
Cyber-Knight <56642237+Cyber-Knightt@users.noreply.github.com>
2025-08-23 13:37:50 +0530

    Time: 6 ms (18.11%) | Memory: 60.3 MB (56.03%) - LeetSync

---
+++ 283-move-zeroes/move-zeroes.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[]} nums
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+var moveZeroes = function(nums) {
+    let lastNonZero = 0;
+    for (let i = 0; i < nums.length; i++) {
+        if (nums[i] !== 0) {
+            // Swap only if needed
+            if (i !== lastNonZero) {
+                [nums[i], nums[lastNonZero]] = [nums[lastNonZero], nums[i]];
+            }
+            lastNonZero++;
+        }
+    }
+};
