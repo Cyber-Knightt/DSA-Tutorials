bebf9bed51cd7f5b396f94f76591d97a095d3276
Cyber-Knight <56642237+Cyber-Knightt@users.noreply.github.com>
2025-08-23 13:28:55 +0530

    Time: 0 ms (100.00%) | Memory: 55.6 MB (59.23%) - LeetSync

--- 263-ugly-number/ugly-number.js
+++ 263-ugly-number/ugly-number.js
@@ -4,13 +4,11 @@
  */
 var isUgly = function (n) {
     if (n <= 0) return false; // must be positive
-
     let factors = [2, 3, 5];
     for (let f of factors) {
         while (n % f === 0) {
             n = n / f;
         }
     }
-
     return n === 1;
 };
