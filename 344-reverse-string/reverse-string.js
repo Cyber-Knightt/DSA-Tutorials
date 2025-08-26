43752833f78b1482d8cc5fc92ed182aa259c36d1
Cyber-Knight <56642237+Cyber-Knightt@users.noreply.github.com>
2025-08-26 10:32:08 +0530

    Time: 0 ms (100.00%) | Memory: 61.8 MB (25.01%) - LeetSync

--- 344-reverse-string/reverse-string.js
+++ 344-reverse-string/reverse-string.js
@@ -5,11 +5,9 @@
 var reverseString = function(s) {
     let left = 0, right = s.length - 1;
     while (left < right) {
-        // swap
         let temp = s[left];
         s[left] = s[right];
         s[right] = temp;
-
         left++;
         right--;
     }
