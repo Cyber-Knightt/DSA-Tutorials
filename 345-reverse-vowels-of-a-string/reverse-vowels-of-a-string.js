026b35c9407c4b9e2e9ef1a7ecc4f86abbf59155
Cyber-Knight <56642237+Cyber-Knightt@users.noreply.github.com>
2025-08-29 08:23:25 +0530

    Time: 8 ms (55.18%) | Memory: 60.3 MB (60.78%) - LeetSync

--- 345-reverse-vowels-of-a-string/reverse-vowels-of-a-string.js
+++ 345-reverse-vowels-of-a-string/reverse-vowels-of-a-string.js
@@ -4,20 +4,16 @@
  */
 var reverseVowels = function (s) {
     let vowels = new Set(['a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U']);
-    let arr = s.split(""); // convert string to array for swapping
+    let arr = s.split("");
     let left = 0, right = arr.length - 1;
 
     while (left < right) {
-        // move left pointer until vowel
         while (left < right && !vowels.has(arr[left])) {
             left++;
         }
-        // move right pointer until vowel
         while (left < right && !vowels.has(arr[right])) {
             right--;
         }
-
-        // swap vowels
         let temp = arr[left];
         arr[left] = arr[right];
         arr[right] = temp;
@@ -25,6 +21,5 @@ var reverseVowels = function (s) {
         left++;
         right--;
     }
-
     return arr.join("");
 };
