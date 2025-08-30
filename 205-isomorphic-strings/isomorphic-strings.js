1547be64a89945ff5361f5bb1805dd1ac1b9c460
Cyber-Knight <56642237+Cyber-Knightt@users.noreply.github.com>
2025-08-30 10:16:00 +0530

    Time: 6 ms (68.25%) | Memory: 54.8 MB (63.98%) - LeetSync

--- 205-isomorphic-strings/isomorphic-strings.js
+++ 205-isomorphic-strings/isomorphic-strings.js
@@ -5,19 +5,15 @@
  */
 var isIsomorphic = function (s, t) {
     if (s.length !== t.length) return false;
-
     let mapST = {}; // s -> t
     let mapTS = {}; // t -> s
-
     for (let i = 0; i < s.length; i++) {
         let c1 = s[i];
         let c2 = t[i];
-
         if (mapST[c1] && mapST[c1] !== c2) return false;
         if (mapTS[c2] && mapTS[c2] !== c1) return false;
         mapST[c1] = c2;
         mapTS[c2] = c1;
     }
-
     return true;
 };
