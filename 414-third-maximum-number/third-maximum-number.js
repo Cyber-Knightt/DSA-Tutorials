cd36aee6c2664053494a9d58fdcccd8cf147ec3f
Cyber-Knight <56642237+Cyber-Knightt@users.noreply.github.com>
2025-09-01 10:41:15 +0530

    Time: 4 ms (48.85%) | Memory: 56 MB (57.24%) - LeetSync

--- 414-third-maximum-number/third-maximum-number.js
+++ 414-third-maximum-number/third-maximum-number.js
@@ -4,14 +4,11 @@
  */
 var thirdMax = function (nums) {
     let uniqueNums = [...new Set(nums)];
-
     // Sort descending
     uniqueNums.sort((a, b) => b - a);
-
     // If less than 3 distinct numbers, return max
     if (uniqueNums.length < 3) {
         return uniqueNums[0];
     }
-
     return uniqueNums[2];
 };
