4b5c4459d227f58cbb365cbfa3c7e82e62421af9
Cyber-Knight <56642237+Cyber-Knightt@users.noreply.github.com>
2025-09-04 16:38:45 +0530

    Time: 20 ms (35.90%) | Memory: 60.8 MB (13.84%) - LeetSync

--- 242-valid-anagram/valid-anagram.js
+++ 242-valid-anagram/valid-anagram.js
@@ -7,12 +7,10 @@ var isAnagram = function(s, t) {
     if (s.length !== t.length) return false;
 
     let map = new Map();
-
     // count frequency of chars in s
     for (let char of s) {
         map.set(char, (map.get(char) || 0) + 1);
     }
-
     // subtract using chars in t
     for (let char of t) {
         if (!map.has(char)) return false;
@@ -21,6 +19,5 @@ var isAnagram = function(s, t) {
             map.delete(char);
         }
     }
-
     return map.size === 0;
 };
