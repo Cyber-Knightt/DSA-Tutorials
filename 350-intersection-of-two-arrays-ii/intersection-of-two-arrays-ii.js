5196597f6e707e334f16a9a560b43e652e3c4667
Cyber-Knight <56642237+Cyber-Knightt@users.noreply.github.com>
2025-09-21 08:31:49 +0530

    Time: 2 ms (57.97%) | Memory: 58.1 MB (5.62%) - LeetSync

--- 350-intersection-of-two-arrays-ii/intersection-of-two-arrays-ii.js
+++ 350-intersection-of-two-arrays-ii/intersection-of-two-arrays-ii.js
@@ -6,19 +6,16 @@
 var intersect = function (nums1, nums2) {
     let map = new Map();
     let result = [];
-
     // Count frequency in nums1
     for (let num of nums1) {
         map.set(num, (map.get(num) || 0) + 1);
     }
 
-    // Check nums2
     for (let num of nums2) {
         if (map.get(num) > 0) {
             result.push(num);
             map.set(num, map.get(num) - 1);
         }
     }
-
     return result;
 };
