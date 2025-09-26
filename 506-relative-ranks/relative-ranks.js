6842c3015c8aa5374ef0ba67a4bf210fcdaaee6c
Cyber-Knight <56642237+Cyber-Knightt@users.noreply.github.com>
2025-09-26 09:15:36 +0530

    Time: 9 ms (69.07%) | Memory: 58.7 MB (54.63%) - LeetSync

--- 506-relative-ranks/relative-ranks.js
+++ 506-relative-ranks/relative-ranks.js
@@ -5,10 +5,8 @@
 var findRelativeRanks = function (score) {
     let arr = score.map((s, i) => [s, i]);
 
-    // Step 2: sort by score descending
     arr.sort((a, b) => b[0] - a[0]);
 
-    // Step 3: prepare result array
     let result = new Array(score.length);
 
     // Step 4: assign ranks
@@ -18,9 +16,7 @@ var findRelativeRanks = function (score) {
         else if (i === 1) rank = "Silver Medal";
         else if (i === 2) rank = "Bronze Medal";
         else rank = (i + 1).toString();
-
         result[arr[i][1]] = rank; // place in original position
     }
-
     return result;
 };
