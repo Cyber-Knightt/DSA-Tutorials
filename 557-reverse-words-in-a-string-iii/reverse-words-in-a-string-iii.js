a542348bc0d7631acacc2351e5d9f4577c319a1b
Cyber-Knight <56642237+Cyber-Knightt@users.noreply.github.com>
2025-09-27 19:17:36 +0530

    Time: 10 ms (65.10%) | Memory: 59.4 MB (91.07%) - LeetSync

---
+++ 557-reverse-words-in-a-string-iii/reverse-words-in-a-string-iii.js
@@ -0,0 +1,10 @@
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var reverseWords = function(s) {
+    return s
+        .split(" ")                // split into words
+        .map(word => word.split("").reverse().join("")) // reverse each word
+        .join(" ");   
+};
