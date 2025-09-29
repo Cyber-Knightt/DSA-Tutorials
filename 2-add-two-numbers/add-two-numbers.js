34a95ee24b2e27bf0ac29bf80fc1e94a69745dcb
Cyber-Knight <56642237+Cyber-Knightt@users.noreply.github.com>
2025-09-29 09:39:55 +0530

    Time: 3 ms (56.48%) | Memory: 61.8 MB (37.96%) - LeetSync

--- 2-add-two-numbers/add-two-numbers.js
+++ 2-add-two-numbers/add-two-numbers.js
@@ -14,9 +14,11 @@ var addTwoNumbers = function (l1, l2) {
     let dummy = new ListNode(0); // dummy head
     let current = dummy;
     let carry = 0;
+    
     while (l1 !== null || l2 !== null || carry > 0) {
         let x = (l1 !== null) ? l1.val : 0;
         let y = (l2 !== null) ? l2.val : 0;
+
         let sum = x + y + carry;
         carry = Math.floor(sum / 10);
 
