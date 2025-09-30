febc020b8f3ec17e9a3a75e1592d15bdb009c277
Cyber-Knight <56642237+Cyber-Knightt@users.noreply.github.com>
2025-09-30 10:58:33 +0530

    Time: 36 ms (85.37%) | Memory: 53.2 MB (72.26%) - LeetSync

--- 278-first-bad-version/first-bad-version.js
+++ 278-first-bad-version/first-bad-version.js
@@ -18,16 +18,16 @@ var solution = function (isBadVersion) {
      * @return {integer} The first bad version
      */
     return function (n) {
-        let left = 1, right = n;
+        let left = 1, right = n
 
         while (left < right) {
-            let mid = Math.floor((left + right) / 2);
+            let mid = Math.floor((left + right) / 2)
             if (isBadVersion(mid)) {
-                right = mid; // first bad version is at mid or before
+                right = mid
             } else {
-                left = mid + 1; // first bad version is after mid
+                left = mid + 1
             }
         }
-        return left;
-    };
+        return left
+    }
 };
