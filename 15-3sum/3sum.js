497484807dc1767d123dbf97d2552bd177bf08cd
Cyber-Knight <56642237+Cyber-Knightt@users.noreply.github.com>
2025-10-04 09:47:26 +0530

    Time: 31 ms (89.68%) | Memory: 70.9 MB (95.16%) - LeetSync

--- 15-3sum/3sum.js
+++ 15-3sum/3sum.js
@@ -3,18 +3,17 @@
  * @return {number[][]}
  */
 var threeSum = function (nums) {
-    let res = [];
-    nums.sort((a, b) => a - b); // Step 1: sort
+    let res = []
+    nums.sort((a, b) => a - b)
 
     for (let i = 0; i < nums.length - 2; i++) {
-        if (i > 0 && nums[i] === nums[i - 1]) continue; // skip duplicates
+        if (i > 0 && nums[i] === nums[i - 1]) continue; 
 
-        let left = i + 1;
+        let left = i + 1
         let right = nums.length - 1;
 
         while (left < right) {
             let sum = nums[i] + nums[left] + nums[right];
-
             if (sum === 0) {
                 res.push([nums[i], nums[left], nums[right]]);
                 left++;
