424ef85b9471032637e7968a0072aff695a51a23
Cyber-Knight <56642237+Cyber-Knightt@users.noreply.github.com>
2025-10-05 10:12:21 +0530

    Time: 18 ms (44.01%) | Memory: 58.8 MB (25.06%) - LeetSync

--- 18-4sum/4sum.js
+++ 18-4sum/4sum.js
@@ -5,14 +5,14 @@
  */
 var fourSum = function (nums, target) {
     let res = [];
-    nums.sort((a, b) => a - b); // Step 1: sort
+    nums.sort((a, b) => a - b);
 
     let n = nums.length;
     for (let i = 0; i < n - 3; i++) {
-        if (i > 0 && nums[i] === nums[i - 1]) continue; // skip dup i
+        if (i > 0 && nums[i] === nums[i - 1]) continue;
 
         for (let j = i + 1; j < n - 2; j++) {
-            if (j > i + 1 && nums[j] === nums[j - 1]) continue; // skip dup j
+            if (j > i + 1 && nums[j] === nums[j - 1]) continue;
 
             let left = j + 1;
             let right = n - 1;
