4a700bb46384eb0e99ee9250216be2ee1651a0d4
Cyber-Knight <56642237+Cyber-Knightt@users.noreply.github.com>
2025-10-08 09:23:44 +0530

    Time: 3 ms (64.04%) | Memory: 59.7 MB (57.54%) - LeetSync

--- 39-combination-sum/combination-sum.js
+++ 39-combination-sum/combination-sum.js
@@ -3,7 +3,7 @@
  * @param {number} target
  * @return {number[][]}
  */
-var combinationSum = function(candidates, target) {
+var combinationSum = function (candidates, target) {
     const result = [];
 
     function backtrack(start, current, total) {
@@ -11,16 +11,13 @@ var combinationSum = function(candidates, target) {
             result.push([...current]); // found a valid combination
             return;
         }
-
         if (total > target) return; // stop exploring
-
         for (let i = start; i < candidates.length; i++) {
             current.push(candidates[i]);
             backtrack(i, current, total + candidates[i]); // reuse allowed
             current.pop(); // backtrack
         }
     }
-
-    backtrack(0, [], 0);
+    backtrack(0, [], 0)
     return result;
 };
