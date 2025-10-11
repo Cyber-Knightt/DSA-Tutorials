cbdf71101e9ab6d85c52d5ba5f481eaa03cb354f
Cyber-Knight <56642237+Cyber-Knightt@users.noreply.github.com>
2025-10-11 15:33:41 +0530

    Time: 2 ms (51.84%) | Memory: 56.6 MB (72.38%) - LeetSync

--- 8-string-to-integer-atoi/string-to-integer-atoi.js
+++ 8-string-to-integer-atoi/string-to-integer-atoi.js
@@ -16,17 +16,14 @@ var myAtoi = function(s) {
         sign = s[i] === '-' ? -1 : 1;
         i++;
     }
-
     // Step 3: Process digits
     while (i < s.length && s[i] >= '0' && s[i] <= '9') {
         result = result * 10 + (s[i].charCodeAt(0) - '0'.charCodeAt(0));
-        
         // Step 4: Check overflow on each iteration
         if (sign * result > 2147483647) return 2147483647;
         if (sign * result < -2147483648) return -2147483648;
         
         i++;
     }
-
     return sign * result;
 };
