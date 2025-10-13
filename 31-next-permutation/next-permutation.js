a49a0d92f6d1f8908800e369b8b6bdb3f700e7f2
Cyber-Knight <56642237+Cyber-Knightt@users.noreply.github.com>
2025-10-13 10:34:38 +0530

    Time: 0 ms (100.00%) | Memory: 55.5 MB (62.00%) - LeetSync

---
+++ 31-next-permutation/next-permutation.js
@@ -0,0 +1,29 @@
+/**
+ * @param {number[]} nums
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+var nextPermutation = function (nums) {
+    // Step 1: Find first decreasing element from right
+    let i = nums.length - 2;
+    while (i >= 0 && nums[i] >= nums[i + 1]) {
+        i--;
+    }
+
+    // Step 2: If found, find next greater element and swap
+    if (i >= 0) {
+        let j = nums.length - 1;
+        while (nums[j] <= nums[i]) {
+            j--;
+        }
+        [nums[i], nums[j]] = [nums[j], nums[i]];
+    }
+
+    // Step 3: Reverse the part after index i
+    let left = i + 1;
+    let right = nums.length - 1;
+    while (left < right) {
+        [nums[left], nums[right]] = [nums[right], nums[left]];
+        left++;
+        right--;
+    }
+};
