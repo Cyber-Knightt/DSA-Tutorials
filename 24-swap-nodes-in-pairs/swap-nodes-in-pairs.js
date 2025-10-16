0f20aae5439307302f0a68c298cc062d715b78aa
Cyber-Knight <56642237+Cyber-Knightt@users.noreply.github.com>
2025-10-16 09:10:13 +0530

    Time: 0 ms (100.00%) | Memory: 55.3 MB (9.31%) - LeetSync

---
+++ 24-swap-nodes-in-pairs/swap-nodes-in-pairs.js
@@ -0,0 +1,34 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var swapPairs = function (head) {
+    // Dummy node before head
+    let dummy = new ListNode(0);
+    dummy.next = head;
+
+    let current = dummy;
+
+    // Traverse while we have at least two nodes ahead
+    while (current.next && current.next.next) {
+        let first = current.next;
+        let second = current.next.next;
+
+        // Swapping nodes
+        first.next = second.next;
+        second.next = first;
+        current.next = second;
+
+        // Move current two nodes ahead
+        current = first;
+    }
+
+    return dummy.next;
+};
