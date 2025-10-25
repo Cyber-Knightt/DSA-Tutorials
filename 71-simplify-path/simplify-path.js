b58007ee3bacf83146feb5c185f6026868138b13
Cyber-Knight <56642237+Cyber-Knightt@users.noreply.github.com>
2025-10-25 14:38:10 +0530

    Time: 0 ms (100.00%) | Memory: 57.5 MB (28.20%) - LeetSync

--- 71-simplify-path/simplify-path.js
+++ 71-simplify-path/simplify-path.js
@@ -15,5 +15,5 @@ var simplifyPath = function (path) {
             stack.push(part); // valid directory name
         }
     }
-    return '/' + stack.join('/');
+    return '/' + stack.join('/')
 };
