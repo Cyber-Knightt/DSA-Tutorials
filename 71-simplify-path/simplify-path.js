e73c02a0df79036ca669cb5dda627d26f74a341f
Cyber-Knight <56642237+Cyber-Knightt@users.noreply.github.com>
2025-10-25 14:34:59 +0530

    Time: 1 ms (88.46%) | Memory: 56.6 MB (49.31%) - LeetSync

---
+++ 71-simplify-path/simplify-path.js
@@ -0,0 +1,20 @@
+/**
+ * @param {string} path
+ * @return {string}
+ */
+var simplifyPath = function (path) {
+    const parts = path.split('/');
+    const stack = [];
+
+    for (let part of parts) {
+        if (part === '' || part === '.') {
+            continue; // ignore empty or current directory
+        } else if (part === '..') {
+            stack.pop(); // go up one directory
+        } else {
+            stack.push(part); // valid directory name
+        }
+    }
+
+    return '/' + stack.join('/');
+};
