af867ca53ee0ca3d278149813d3f406cb23d69b6
Cyber-Knight <56642237+Cyber-Knightt@users.noreply.github.com>
2025-10-31 11:01:40 +0530

    Time: 45 ms (95.67%) | Memory: 58.3 MB (90.62%) - LeetSync

--- 141-linked-list-cycle/linked-list-cycle.js
+++ 141-linked-list-cycle/linked-list-cycle.js
@@ -19,9 +19,9 @@ var hasCycle = function (head) {
     while (fast && fast.next) {
         slow = slow.next;        // move 1 step
         fast = fast.next.next;   // move 2 steps
-
         if (slow === fast) {     // they meet — cycle detected
             return true;
         }
-    }return false;
+    }
+    return false;
 };
