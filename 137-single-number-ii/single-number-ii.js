b59241caec4aedb68006796c843b11cea079ab27
Cyber-Knight <56642237+Cyber-Knightt@users.noreply.github.com>
2025-11-04 10:53:40 +0530

    Time: 3 ms (81.33%) | Memory: 56.4 MB (57.11%) - LeetSync

---
+++ 137-single-number-ii/single-number-ii.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var singleNumber = function (nums) {
+    let ones = 0;  // bits seen 1 time mod 3
+    let twos = 0;  // bits seen 2 times mod 3
+
+    for (const x of nums) {
+        ones = (ones ^ x) & ~twos;
+        twos = (twos ^ x) & ~ones;
+    }
+    return ones; 
+};
