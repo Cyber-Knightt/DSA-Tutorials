2945fc635a58890a584d12f104c41e94b11cec3a
Cyber-Knight <56642237+Cyber-Knightt@users.noreply.github.com>
2025-11-05 09:39:40 +0530

    Time: 0 ms (100.00%) | Memory: 66.6 MB (22.81%) - LeetSync

---
+++ 134-gas-station/gas-station.js
@@ -0,0 +1,25 @@
+/**
+ * @param {number[]} gas
+ * @param {number[]} cost
+ * @return {number}
+ */
+var canCompleteCircuit = function (gas, cost) {
+    let totalTank = 0;    // total net gas for the whole circuit
+    let currTank = 0;     // gas in tank for the current start
+    let startStation = 0; // candidate starting index
+
+    for (let i = 0; i < gas.length; i++) {
+        const diff = gas[i] - cost[i];
+        totalTank += diff;
+        currTank += diff;
+
+        // If we can’t reach the next station, reset start
+        if (currTank < 0) {
+            startStation = i + 1; // try next station
+            currTank = 0;         // reset current tank
+        }
+    }
+
+    // If total gas < total cost, no possible start
+    return totalTank >= 0 ? startStation : -1;
+};
