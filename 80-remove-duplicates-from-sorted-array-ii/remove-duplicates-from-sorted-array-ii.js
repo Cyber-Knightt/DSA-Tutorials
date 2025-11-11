53ac263686ecdf61197fae8a6d5fb030cf3c694c
Cyber-Knight <56642237+Cyber-Knightt@users.noreply.github.com>
2025-11-11 09:14:26 +0530

    Time: 56 ms (68.47%) | Memory: 59.5 MB (19.53%) - LeetSync

---
+++ 80-remove-duplicates-from-sorted-array-ii/remove-duplicates-from-sorted-array-ii.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var removeDuplicates = function (nums) {
+    if (nums.length <= 2) return nums.length;
+    
+    let i = 2; // start from index 2 since first two can always stay
+    for (let j = 2; j < nums.length; j++) {
+        // compare current element with the element 2 positions before
+        if (nums[j] !== nums[i - 2]) {
+            nums[i] = nums[j];
+            i++;
+        }
+    }
+    return i;
+};
