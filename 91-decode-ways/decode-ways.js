bf4c23c6e0c5afe3ae1ac4e23a0a897381e82e37
Cyber-Knight <56642237+Cyber-Knightt@users.noreply.github.com>
2025-11-13 10:02:00 +0530

    Time: 1 ms (60.72%) | Memory: 55.4 MB (64.22%) - LeetSync

--- 91-decode-ways/decode-ways.js
+++ 91-decode-ways/decode-ways.js
@@ -3,7 +3,7 @@
  * @return {number}
  */
 var numDecodings = function (s) {
-    if (!s || s[0] === '0') return 0;  // can't start with 0
+    if (!s || s[0] === '0') return 0;
 
     const n = s.length;
     const dp = new Array(n + 1).fill(0);
@@ -13,15 +13,13 @@ var numDecodings = function (s) {
     for (let i = 2; i <= n; i++) {
         const oneDigit = Number(s.slice(i - 1, i));
         const twoDigits = Number(s.slice(i - 2, i));
-
         // Single digit valid (1–9)
         if (oneDigit >= 1 && oneDigit <= 9) {
             dp[i] += dp[i - 1];
         }
-
-        // Two digits valid (10–26)
         if (twoDigits >= 10 && twoDigits <= 26) {
             dp[i] += dp[i - 2];
         }
-    }return dp[n];
+    }
+    return dp[n];
 };
