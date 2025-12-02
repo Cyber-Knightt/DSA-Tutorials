22f1fa372e0ea9cae07a694c5d283a755609c6f6
Cyber-Knight <56642237+Cyber-Knightt@users.noreply.github.com>
2025-12-02 11:12:03 +0530

    Time: 0 ms (100.00%) | Memory: 56 MB (16.12%) - LeetSync

---
+++ 59-spiral-matrix-ii/spiral-matrix-ii.js
@@ -0,0 +1,44 @@
+/**
+ * @param {number} n
+ * @return {number[][]}
+ */
+var generateMatrix = function (n) {
+    let matrix = Array.from({ length: n }, () => Array(n).fill(0));
+
+    let top = 0, bottom = n - 1;
+    let left = 0, right = n - 1;
+    let num = 1;
+
+    while (top <= bottom && left <= right) {
+
+        // left → right
+        for (let i = left; i <= right; i++) {
+            matrix[top][i] = num++;
+        }
+        top++;
+
+        // top → bottom
+        for (let i = top; i <= bottom; i++) {
+            matrix[i][right] = num++;
+        }
+        right--;
+
+        // right → left
+        if (top <= bottom) {
+            for (let i = right; i >= left; i--) {
+                matrix[bottom][i] = num++;
+            }
+            bottom--;
+        }
+
+        // bottom → top
+        if (left <= right) {
+            for (let i = bottom; i >= top; i--) {
+                matrix[i][left] = num++;
+            }
+            left++;
+        }
+    }
+
+    return matrix;
+};
